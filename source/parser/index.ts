2f4879b72ae872cfc4d75a1541373cc8e49a9f5e
Henry Hale <henryhale@users.noreply.github.com>
2023-10-02 18:57:08 -0500

    refactor: updated type imports

--- source/parser/index.ts
+++ source/parser/index.ts
@@ -1,6 +1,6 @@
 import { parseArgs } from "./input";
 import { parseCommand } from "./commands";
-import { ParsedCommand } from "../interface";
+import type { ParsedCommand } from "../interface";
 
 export function parseInputIntoCommands(str: string): ParsedCommand[] {
     return parseArgs(str).map((args) => parseCommand(args));
